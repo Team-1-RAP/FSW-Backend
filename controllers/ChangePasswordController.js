e588f898d900b1e9139dd25f3a610793c23aa335
YulinarNR <yulinarnurrahma@gmail.com>
2024-08-01 17:22:14 +0700

    feat: email validation

--- controllers/ChangePasswordController.js
+++ controllers/ChangePasswordController.js
@@ -3,6 +3,8 @@ import Customer from '../models/customer.js';
 import FlagUser from '../models/flagUser.js';
 import Account from '../models/account.js';
 import { formatToJakartaTime } from "../utils/dateUtils.js";
+import { sendOTPEmail } from "../utils/emailUtils.js";
+import { generateOTP } from "../controllers/accountController.js";
 
 export const currentPassword = async (req, res) => {
     const { current_password } = req.body;
@@ -83,6 +85,115 @@ export const currentPassword = async (req, res) => {
         });
     } catch (error) {
         console.error('Error:', error);
+        return res.status(500).json({
+            code: 500,
+            message: 'Internal server error',
+            data: null
+        });
+    }
+};
+
+export const validateEmail = async (req, res) => {
+    const { email } = req.body; 
+
+    try {
+        if (!email) { 
+            return res.status(404).json({
+                code: 404,
+                message: 'Email needed',
+                status: false,
+                data: null
+            });
+        }
+
+        const flagUser = await FlagUser.findOne({ where: { customer_id: req.user.userId } });
+
+        if (!flagUser || flagUser.is_currentPass_valid !== true) {
+            return res.status(400).json({
+                code: 400,
+                message: 'Current password validation not completed or failed',
+                status: false,
+                data: null
+            });
+        }
+
+        const customer = await Customer.findOne({ where: { id: req.user.userId, email: email } });
+
+        if (!customer) {
+            return res.status(400).json({
+                code: 400,
+                message: 'Email does not match user data',
+                status: false,
+                data: null
+            });
+        }
+
+        const account = await Account.findOne({ where: { userId: customer.id } });
+
+        if (!account) {
+            return res.status(404).json({
+                code: 404,
+                message: 'Account not found for the customer',
+                status: false,
+                data: null
+            });
+        }
+
+        const otp = generateOTP();
+        const otpExpiry = new Date(Date.now() + 10 * 60 * 1000).toISOString(); 
+
+        const name = customer.fullname || 'Pengguna SimpleBank';
+        await sendOTPEmail(email, otp, name);
+
+        await FlagUser.update(
+            {
+                is_email_valid: true,
+                otp: otp,
+                otp_expired_date: otpExpiry,
+                updated_at: new Date().toISOString() 
+            },
+            { where: { customer_id: req.user.userId } }
+        );
+
+        const updatedFlagUser = await FlagUser.findOne({ where: { customer_id: req.user.userId } });
+        
+        const { updated_at: updatedFlagUserUpdatedAt, otp_expired_date: otpExpiredDateFlagUser } = updatedFlagUser;
+        const updatedAtFormatted = formatToJakartaTime(updatedFlagUserUpdatedAt);
+        const otpExpiredFormatted = formatToJakartaTime(otpExpiredDateFlagUser);
+
+        return res.status(200).json({
+            code: 200,
+            message: 'Email validation success, check your email for OTP code',
+            data: {
+                atm_card_no: account.atm_card_no,
+                account_no: account.no,
+                customer_id: customer.id,
+                account_type: account.accountType,
+                balance: account.balance,
+                customer_data: {
+                    id: customer.id,
+                    username: customer.username,
+                    fullname: customer.fullname,
+                    email: customer.email,
+                    born_date: customer.bornDate,
+                },
+                flag_user: {
+                    is_currentPass_valid: updatedFlagUser.is_currentPass_valid,
+                    is_email_valid: updatedFlagUser.is_email_valid,
+                    is_verified: updatedFlagUser.is_verified,
+                    updated_at: updatedAtFormatted
+                },
+                otp_code: {
+                    otp: updatedFlagUser.otp,
+                    otp_expired_date: otpExpiredFormatted
+                }
+            },
+            stepValidation: 2,
+            created_date: account.createdDate
+        });
+
+    } catch (error) {
+        console.error('Error email validation:', error);
         return res.status(500).json({
             code: 500,
             message: 'Internal server error',
--- routes/ChangePassword.js
+++ routes/ChangePassword.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentPassword } from '../controllers/ChangePasswordController.js';
+import { currentPassword, validateEmail } from '../controllers/ChangePasswordController.js';
 import { verifyToken } from '../middleware/VerifyToken.js';
 
 const router = express.Router();
@@ -23,6 +23,22 @@ const router = express.Router();
  *       type: http
  *       scheme: bearer
  *       bearerFormat: JWT
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     EmailValidationChangeRequest:
+ *       type: object
+ *       required:
+ *         - email
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: The email address to be validated
+ *       example:
+ *         email: "user@gmail.com"
  */
 
 /**
@@ -65,6 +81,40 @@ const router = express.Router();
  *                   description: Additional data
  */
 
+/**
+ * @swagger
+ * /v1/change/password/validation/email:
+ *   post:
+ *     summary: 
+ *     tags: [Change Password]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/EmailValidationChangeRequest'
+ *     responses:
+ *       200:
+ *         description: Email validation successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   description: Response code
+ *                 message:
+ *                   type: string
+ *                   description: Response message
+ *                 data:
+ *                   type: object
+ *                   description: Additional data
+ */
+
 router.post('/v1/change/password/validation/currentPassword', verifyToken, currentPassword);
+router.post('/v1/change/password/validation/email', verifyToken, validateEmail);
 
 export default router;
