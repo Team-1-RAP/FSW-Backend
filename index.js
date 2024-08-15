3fe5cb3c3f89d32345bdba09952d6512334cbd02
YulinarNR <yulinarnurrahma@gmail.com>
2024-08-15 14:16:34 +0700

    refactor: controller name and add get all account purposes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import ResetPinRoute from './src/routes/ResetPinRoutes.js';
 import BankRoutes from './src/routes/BankRoutes.js';
 import ChangePasswordRoutes from './src/routes/ChangePasswordRoutes.js';
 import RegistCustomerRoutes from './src/routes/RegistCustomerRoutes.js';
-import AccountTypeRoutes from './src/routes/AccountTypeRoutes.js';
+import AccountRoutes from './src/routes/AccountRoutes.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger.js';
 
@@ -23,7 +23,7 @@ app.use(ResetPinRoute);
 app.use(BankRoutes);
 app.use(ChangePasswordRoutes);
 app.use(RegistCustomerRoutes);
-app.use(AccountTypeRoutes);
+app.use(AccountRoutes);
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
