cc559fc81cc3f4f4e3ad74e33a61cf8c6ec0a42e
YulinarNR <yulinarnurrahma@gmail.com>
2024-08-15 20:13:25 +0700

    chore: update utils for validation global

--- src/controllers/RegistCustomerController.js
+++ src/controllers/RegistCustomerController.js
@@ -7,38 +7,7 @@ import { generateOTP } from "../utils/generateOtpUtils.js";
 import { formatToJakartaTime } from "../utils/dateUtils.js"
 import AccountTypes from '../models/AccountTypes.js';
 import sequelize from '../config/config.js';
-
-const EMAIL_FORMAT = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const USERNAME_FORMAT = /^(?!\d+$)[A-Za-z0-9]{6}$/;
-const PASSWORD_FORMAT = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/;
-const PIN_FORMAT = /^[0-9]{6}$/;
-
-const validateEmail = (email) => EMAIL_FORMAT.test(email);
-
-const validateUsername = (username) => USERNAME_FORMAT.test(username);
-
-const validatePassword = (password, confirmPassword) => {
-    if (password !== confirmPassword) {
-        return { valid: false, message: 'Password and confirm password do not match' };
-    }
-    if (password.length < 8) {
-        return { valid: false, message: 'Password must be at least 8 characters' };
-    }
-    if (!PASSWORD_FORMAT.test(password)) {
-        return { valid: false, message: 'Password must contain at least one uppercase letter, one number, and one special character or symbol' };
-    }
-    return { valid: true };
-};
-
-const validatePin = (pin, confirmPin) => {
-    if (pin !== confirmPin) {
-        return { valid: false, message: 'Pin and confirm pin do not match' };
-    }
-    if (!PIN_FORMAT.test(pin)) {
-        return { valid: false, message: 'Pin must be at least 6 digits' };
-    }
-    return { valid: true };
-};
+import { validateEmail, validateUsername, validatePassword, validatePin } from "../utils/validationUtils.js";
 
 export const registrationAccount = async (req, res) => {
     const { email, username, password, confirmPassword } = req.body;
