a0f603d32a34f57db1a45ddfd816b0fcb0de50f1
YulinarNR <yulinarnurrahma@gmail.com>
2024-08-23 21:14:35 +0700

    swagger

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const options = {
         info: {
             title: 'Simple Bank API - BE FSW',
             version: '1.0.0',
-            description: '[/api/v3/api-docs](http://localhost:5000/api/v3/api-docs)',
+            description: '[/api/v3/api-docs](https://simplebank.my.id/api/v3/api-docs)',
         },
         servers: [
             {
