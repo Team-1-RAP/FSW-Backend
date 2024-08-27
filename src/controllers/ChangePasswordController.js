7351725aa988335815377f7fb0d072bdce235b6a
YulinarNR <yulinarnurrahma@gmail.com>
2024-08-27 20:30:43 +0700

    refactor: email validation step

--- src/controllers/ChangePasswordController.js
+++ src/controllers/ChangePasswordController.js
@@ -6,7 +6,7 @@ import { formatToJakartaTime } from '../utils/dateUtils.js';
 import { sendOTPEmail } from '../utils/emailUtils.js';
 import { generateOTP } from '../utils/generateOtpUtils.js';
 import { validatePassword } from '../utils/validationUtils.js';
-import { currentPasswordStep } from '../service/changePasswordService.js';
+import { currentPasswordStep, validateEmailStep } from '../service/changePasswordService.js';
 import { sendResponse, sendErrResponse } from '../helpers/responseHelper.js';
 
 export const currentPassword = async (req, res) => {
@@ -25,111 +25,17 @@ export const currentPassword = async (req, res) => {
 };
 
 export const validateEmail = async (req, res) => {
-    const { email } = req.body; 
-
     try {
-        if (!email) { 
-            return res.status(404).json({
-                code: 404,
-                message: 'Email needed',
-                status: false,
-                data: null
-            });
+        const result = await validateEmailStep(req.body, req.user.userId)
+
+        if (result.error) {
+            return sendResponse (res, result.status, result.message, false, null)
         }
 
-        const flagUser = await FlagUser.findOne({ where: { customer_id: req.user.userId } });
-
-        if (!flagUser || flagUser.is_currentPass_valid !== true) {
-            return res.status(400).json({
-                code: 400,
-                message: 'Current password validation not completed or failed',
-                status: false,
-                data: null
-            });
-        }
-
-        const customer = await Customer.findOne({ where: { id: req.user.userId, email: email } });
-
-        if (!customer) {
-            return res.status(400).json({
-                code: 400,
-                message: 'Email does not match user data',
-                status: false,
-                data: null
-            });
-        }
-
-        const account = await Account.findOne({ where: { userId: customer.id } });
-
-        if (!account) {
-            return res.status(404).json({
-                code: 404,
-                message: 'Account not found for the customer',
-                status: false,
-                data: null
-            });
-        }
-
-        const otp = generateOTP();
-        const otpExpiry = new Date(Date.now() + 10 * 60 * 1000).toISOString(); 
-
-        const name = customer.fullname || 'Pengguna SimpleBank';
-        await sendOTPEmail(email, otp, name);
-
-        await FlagUser.update(
-            {
-                is_email_valid: true,
-                otp: otp,
-                otp_expired_date: otpExpiry,
-                updated_at: new Date().toISOString() 
-            },
-            { where: { customer_id: req.user.userId } }
-        );
-
-        const updatedFlagUser = await FlagUser.findOne({ where: { customer_id: req.user.userId } });
-        
-        const { updated_at: updatedFlagUserUpdatedAt, otp_expired_date: otpExpiredDateFlagUser } = updatedFlagUser;
-        const updatedAtFormatted = formatToJakartaTime(updatedFlagUserUpdatedAt);
-        const otpExpiredFormatted = formatToJakartaTime(otpExpiredDateFlagUser);
-
-        return res.status(200).json({
-            code: 200,
-            message: 'Email validation success, check your email for OTP code',
-            data: {
-                atm_card_no: account.atm_card_no,
-                account_no: account.no,
-                customer_id: customer.id,
-                account_type: account.accountType,
-                balance: account.balance,
-                customer_data: {
-                    id: customer.id,
-                    username: customer.username,
-                    fullname: customer.fullname,
-                    email: customer.email,
-                    born_date: customer.bornDate,
-                },
-                flag_user: {
-                    is_currentPass_valid: updatedFlagUser.is_currentPass_valid,
-                    is_email_valid: updatedFlagUser.is_email_valid,
-                    is_verified: updatedFlagUser.is_verified,
-                    updated_at: updatedAtFormatted
-                },
-                otp_code: {
-                    otp: updatedFlagUser.otp,
-                    otp_expired_date: otpExpiredFormatted
-                }
-            },
-            stepValidation: 2,
-            created_date: account.createdDate
-        });
-
-    } catch (error) {
-        console.error('Error email validation:', error);
-        return res.status(500).json({
-            code: 500,
-            message: 'Internal server error',
-            data: null
-        });
+        return sendResponse(res, 200, 'Email validation success, check your email for OTP code', true, result.data);
+    } catch(error) {
+        console.error('Error:', error);
+        return sendErrResponse(res, 500, 'Internal server error', false, { error: error.message });
     }
 };
 
--- src/service/changePasswordService.js
+++ src/service/changePasswordService.js
@@ -3,9 +3,20 @@ import Customer from '../models/Customers.js';
 import FlagUser from '../models/FlagUsers.js';
 import Account from '../models/Accounts.js';
 import { formatToJakartaTime } from "../utils/dateUtils.js";
+import { generateOTP } from '../utils/generateOtpUtils.js';
+import { sendOTPEmail } from '../utils/emailUtils.js';
+import { handleError } from '../utils/handleError.js';
 
 const getCustomer = async (userId) => {
     return await Customer.findOne({ where: { id: userId }});
+}
+
+const getFlagUser = async (userId) => {
+    return await FlagUser.findOne({ where: {customer_id: userId}})
+}
+
+const getUserAccount = async (customerId) => {
+    return await Account.findOne({ where: { userId: customerId } });
 }
 
 export const currentPasswordStep = async (body, userId) => {
@@ -67,11 +78,78 @@ export const currentPasswordStep = async (body, userId) => {
             }
         };
     } catch (error) {
-        console.error('Error in validateCurrentPassword:', error);
+        return handleError(error);
+    }
+};
+
+export const validateEmailStep = async (body, userId) => {
+    const { email } = body; 
+
+    try {
+        if (!email) return { error: true, status: 400, message: 'Email needed' };
+
+        const flagUser = await getFlagUser(userId);
+        if (!flagUser || flagUser.is_currentPass_valid !== true) return { error: true, status: 400, message: 'Current password validation not completed or failed' };
+
+        const customer = await Customer.findOne({ where: { id: userId, email: email } });
+        if (!customer) return { error: true, status: 400, message: 'Email does not match user data' };
+
+        const account = await getUserAccount(customer.id);
+        if (!account) return { error: true, status: 400, message: 'Account not found for the customer' };
+
+        const otp = generateOTP();
+        const otpExpiry = new Date(Date.now() + 10 * 60 * 1000).toISOString(); 
+
+        const name = customer.fullname || 'Pengguna SimpleBank';
+        await sendOTPEmail(email, otp, name);
+
+        await FlagUser.update(
+            {
+                is_email_valid: true,
+                otp: otp,
+                otp_expired_date: otpExpiry,
+                updated_at: new Date().toISOString() 
+            },
+            { where: { customer_id: userId } }
+        );
+
+        const updatedFlagUser = await FlagUser.findOne({ where: { customer_id: userId } });
+        
+        const { updated_at: updatedFlagUserUpdatedAt, otp_expired_date: otpExpiredDateFlagUser } = updatedFlagUser;
+        const updatedAtFormatted = formatToJakartaTime(updatedFlagUserUpdatedAt);
+        const otpExpiredFormatted = formatToJakartaTime(otpExpiredDateFlagUser);
+
         return {
-            error: true,
-            status: 500,
-            message: 'Internal server error',
+            error: false,
+            data: {
+                atm_card_no: account.atm_card_no,
+                account_no: account.no,
+                customer_id: customer.id,
+                account_type: account.accountType,
+                balance: account.balance,
+                customer_data: {
+                    id: customer.id,
+                    username: customer.username,
+                    fullname: customer.fullname,
+                    email: customer.email,
+                    born_date: customer.bornDate,
+                },
+                flag_user: {
+                    is_currentPass_valid: updatedFlagUser.is_currentPass_valid,
+                    is_email_valid: updatedFlagUser.is_email_valid,
+                    is_verified: updatedFlagUser.is_verified,
+                    updated_at: updatedAtFormatted
+                },
+                otp_code: {
+                    otp: updatedFlagUser.otp,
+                    otp_expired_date: otpExpiredFormatted
+                }
+            },
+            stepValidation: 2,
+            created_date: account.createdDate
         };
+
+    } catch (error) {
+        return handleError(error);
     }
 };
